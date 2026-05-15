e5058767edf181d5807626e12407c9692bbde8bb
Deine Mutter <milf_hunter_fucker_2000@leck_eier.de>
2026-05-15 05:02:39 +0000

    update

--- minimal_solutions/minecraft_normalizer/minecraft_normalizer_validation.py
+++ minimal_solutions/minecraft_normalizer/minecraft_normalizer_validation.py
@@ -4,9 +4,19 @@ def validate_input(data):
     rules = {
         "input_text": {
             "required": True,
-            "type": str,
+            "type": (str, list),
             "min_length": 1,
             "max_length": 5000
+        },
+        "mode": {
+            "required": False,
+            "type": str,
+            "min_length": 1,
+            "max_length": 50
+        },
+        "options": {
+            "required": False,
+            "type": dict
         }
     }
     return Validator.validate(data, rules)
