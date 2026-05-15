9b3e527aa65c00932c86d896e5420eeff0537a06
Deine Mutter <milf_hunter_fucker_2000@leck_eier.de>
2026-05-15 05:50:17 +0000

    update

--- minimal_solutions/minecraft_mapper/minecraft_mapper_validation.py
+++ minimal_solutions/minecraft_mapper/minecraft_mapper_validation.py
@@ -1,6 +1,22 @@
-"""
-Minecraft Mapper Validation
-"""
+from app.validation import Validator
 
-def validate_request(data):
-    return True, ""
+def validate_input(data):
+    rules = {
+        "input_text": {
+            "required": True,
+            "type": (str, list),
+            "min_length": 1,
+            "max_length": 5000
+        },
+        "mode": {
+            "required": False,
+            "type": str,
+            "min_length": 1,
+            "max_length": 50
+        },
+        "options": {
+            "required": False,
+            "type": dict
+        }
+    }
+    return Validator.validate(data, rules)
